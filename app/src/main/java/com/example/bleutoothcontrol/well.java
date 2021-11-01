1943e58458e221bfe82567190fbe3788f936878a
Grioui-Hanane <61666394+Grioui-Hanane@users.noreply.github.com>
2021-11-01 12:49:50 -0700

    Practice Assignment

---
+++ app/src/main/java/com/example/bleutoothcontrol/well.java
@@ -0,0 +1,33 @@
+package com.example.bleutoothcontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.os.AsyncTask;
+import java.io.IOException;
+import java.util.UUID;
+
+public class well extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        }
+
+    }
+
+
